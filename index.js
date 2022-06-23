db650c0ec7a314a45e5b4adf70ebc576b234eede
Jose Montoya <josem@Joses-MacBook-Pro.local>
2022-06-22 21:05:03 -0700

    trivia

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your Code Here
+let userName = window.prompt('Please enter your name:')
+
+for(let i = 0; i < questions.length; i++) {
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    console.log(userAnswer)
+}
